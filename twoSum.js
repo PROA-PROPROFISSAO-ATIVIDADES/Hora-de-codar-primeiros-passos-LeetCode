03a185664a373d29d59749c0c86f75b6212aaead
JOSE VICTOR XAVIER SILVA <jose.vxsilva@senacsp.edu.br>
2026-08-13 12:42:01 -0300

    first steap leet code

---
+++ twoSum.js
@@ -0,0 +1,17 @@
+function twoSum(nums, target) {
+    const map = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        const complemento = target - nums[i];
+        
+        if (map.has(complemento)) {
+            return [map.get(complemento), i];
+        }
+        
+        map.set(nums[i], i);
+
+        console.log(map)
+    }
+}
+
+console.log(twoSum([3, 2, 4], 6))
